b314a3e943cfbba7bb7a8b369bfde6ceeb1ffec4
abuelocarl <carl.j.calegari@gmail.com>
2026-03-06 11:57:51 -0500

    Initial Commit

---
+++ Fitness Hydration Tracker/Fitness_Hydration_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_Hydration_TrackerApp.swift
+//  Fitness Hydration Tracker
+//
+//  Created by Carlos Calegari on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_Hydration_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fitness Hydration TrackerTests/Fitness_Hydration_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_Hydration_TrackerTests.swift
+//  Fitness Hydration TrackerTests
+//
+//  Created by Carlos Calegari on 3/6/26.
+//
+
+import Testing
+@testable import Fitness_Hydration_Tracker
+
+struct Fitness_Hydration_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
